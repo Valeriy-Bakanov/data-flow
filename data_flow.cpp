2db6683869931c08e4cedd4c1c01828182a7481a
Valery-Bakanov <e881e@mail.ru>
2020-09-24 10:08:21 +0300

    It's_commit_of_DATA-FLOW_system-24.09.2020_10:08:21,79

--- data_flow.cpp
+++ data_flow.cpp
@@ -11,6 +11,7 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         {
                  Application->Initialize();
                  Application->Title = " —имул€ци€ DATA-FLOW вычислител€";
+                 Application->HelpFile = "";
                  Application->CreateForm(__classid(TF1), &F1);
                  Application->CreateForm(__classid(TF2), &F2);
                  Application->CreateForm(__classid(TF3), &F3);
