6f6508c8eb5e014e0c60e5a5f0f068d9708c4c4b
Valery-Bakanov <e881e@mail.ru>
2020-12-29 03:43:26 +0300

    It's_commit_of_DATA-FLOW_system-29.12.2020_ 3:43:26,30

--- Unit1.cpp
+++ Unit1.cpp
@@ -65,7 +65,7 @@ char *df_pt0  = "=\\d{1,}/\\d{1,}:", // =12/155: // шаблоны для регулярных выраж
 #define strcat(dest,src) (strncat(dest,src,sizeof(dest)-strlen(dest)-5)) // безопасное добавление src к dest
 // !!!!! здесь 5 - просто дополнительная защита ("на всякий случай") !!!!!!!!!!!
 //
-char Ident[] = "Bakanov Valery Mikhailovich, Moscow, Russia, 2009-2020\n \
+char Ident[] = "Bakanov Valery Mikhailovich, Moscow, Russia, 2009-2021\n \
 Автор программного продукта Валерий Баканов не является ярым сторонником Объектно-Ориентированного Программирования \
 (внутри автора точно сидит Линус Торвальдс!) и поэтому большая часть кода не использует ООП\n \
 The author of the software Valery Bakanov is not an ardent supporter of Object-Oriented Programming \
